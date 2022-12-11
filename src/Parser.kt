46126bf7f565e5c8078a57fe64e516760fef587a
Sma5aka <54479462+Sma5aka@users.noreply.github.com>
2022-12-11 23:41:42 +1000

    lexer is done_0.6

--- src/Parser.kt
+++ src/Parser.kt
@@ -1,13 +1,59 @@
-class Node
+import java.io.FileInputStream
+open class Node
+data class BinOp(val op: String, val left: Node, val right: Node): Node()
+data class Number<T>(val value: T): Node()
+data class Variable(val name: String): Node()
 
-enum class Operation
+class Parser (val dir: FileInputStream, val lexems: MutableList<Lexem>) {
+    var vars = mutableMapOf<String,Any>()
+    val lexer: Lexer = Lexer(dir)
+    fun ParseExpression(): Node {
+        var left = ParseTerm()
+        var lex = if (lexer.iterator().hasNext()) lexer.iterator().next() else Lexem(0,0, Types.ERR, "Error")
+        println(lex)
+        while (lex.value in mutableListOf("+", "-")){
+            left = BinOp(lex.value, left, ParseExpression())
+            lex = if (lexer.iterator().hasNext()) lexer.iterator().next() else Lexem(0,0, Types.ERR, "Error")
+        }
+        return left
+    }
 
-data class BinOp (val op: Operation, val left: Node, val right: Node)
+    fun ParseTerm(): Node {
+        var left = ParseFactor()
+        var lex = if (lexer.iterator().hasNext()) lexer.iterator().next() else Lexem(0,0, Types.ERR, "Error")
+        println(lex)
+        while (lex.value in mutableListOf("*", "/")){
+            left = BinOp(lex.value, left, ParseTerm())
+            lex = if (lexer.iterator().hasNext()) lexer.iterator().next() else Lexem(0,0, Types.ERR, "Error")
+        }
+        return left
+    }
 
-data class Number<T> (val value: T)
+    fun ParseFactor(): Node {
+        var lex = if (lexer.iterator().hasNext()) lexer.iterator().next() else Lexem(0,0, Types.ERR, "Error")
+        println(lex)
+        if (lex.type == Types.FLOAT) {
 
-data class Variable (val name: String)
+            return Number<Float>(lex.value.toFloat())
+        }
+        if (lex.type == Types.INT) {
 
-class Parser {
-    private val in_lexems: MutableList<Lexem> = mutableListOf()
+            return Number<Int>(lex.value.toInt())
+        }
+        if (lex.type == Types.ID) {
+
+            return Variable(lex.value)
+        }
+        return Node()
+        /*if (lex.value == "("){
+            token++
+            var e = ParseExpression()
+            if (lexems[token].value != ")"){
+                return "No right bracket"
+            }
+            token++
+            return e
+        }
+        return "Factor expected"*/
+    }
 }
--- src/source.kt
+++ src/source.kt
@@ -2,46 +2,21 @@ import java.io.File
 import java.io.FileInputStream
 
 fun main(args: Array<String>){
-    /*if (args.indexOf("-lex") != -1){
-        lex_analys(args[1])
+    /*if ("-lex" in args){
+        lex_analys(args[1], "6")
     }*/
-    testing(args[1])
 
-    /*val stream: FileInputStream = FileInputStream(args[1])
-    for (i in Lexer(stream)){
-        println("${i.pos_x}\t${i.pos_y}\t${i.type}\t${i.value}")
-    }*/
-}
-
-fun testing(dir: String){
-
-    var reader1 : FileInputStream
-    var reader2 : FileInputStream
-
-    var i: Int = 1
-    while (i != 6){
-        var buffer1: String = ""
-        var buffer2: String = ""
-
-        lex_analys(dir+"$i.in", i.toString())
-
-        reader1 = FileInputStream(dir+"$i.out")
-        reader2 = FileInputStream("tests\\answ\\$i.txt")
-        var iter: Int
-        while (reader1.read().also { iter = it } != -1) {
-            buffer1 += iter.toChar()
-        }
-        while (reader2.read().also { iter = it } != -1) {
-            buffer2 += iter.toChar()
-        }
-
-        if(buffer1 == buffer2){
-            println("$i - TRUE")
-        } else {
-            println("$i - FALSE")
-        }
-        i++
+    val Lexems: MutableList<Lexem> = mutableListOf()
+    val str: FileInputStream = FileInputStream(args[1])
+    for (lex in Lexer(str)){
+        Lexems.add(lex)
     }
+    print(Lexems)
+    //val lexer: Lexer = Lexer(str)
+
+    var parser: Parser = Parser(str, Lexems)
+    var e = parser.ParseExpression()
+    print(e)
 }
 
 fun lex_analys(_name: String, _fname: String) {
@@ -51,6 +26,9 @@ fun lex_analys(_name: String, _fname: String) {
 
     if(ne){
         for (i in Lexer(str)) {
+
+            print("${i.pos_x}\t${i.pos_y}\t${i.type}\t${i.value}\n") // For debug
+
             ne = false
             val output: File = File("tests/inout/${fname}.out")
             output.appendText("${i.pos_x}\t${i.pos_y}\t${i.type}\t${i.value}\n")
